0ab61ca467176f86ed62b0b941cf1bcbefaa9e2d
zul <zul.zulkarnaen@gmail.com>
2016-12-23 10:31:21 +0700

    Implement i3 layout for taskwarrior

--- scripts/taskwarrior.sh
+++ scripts/taskwarrior.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec urxvt -e zsh -c 'task logo;task long;zsh'
+exec urxvt -title "taskwarrior" -e zsh -c 'task;tasksh' &
+exec urxvt -title "tty-clock" -e zsh -c 'tty-clock -c -s' &
+exec urxvt -title "logo" -e zsh -c 'task logo;task calendar;zsh' &
