3c5cca3da964d408ebecb37633b840b042e14948
zul <zul.zulkarnaen@gmail.com>
2017-01-04 10:17:19 +0000

    Updated app list

--- scripts/install-app.sh
+++ scripts/install-app.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Basic
 sudo pacman -S task gvim chromium rxvt-unicode
-sudo pacman -S ranger
+# File Manager
+sudo pacman -S ranger w3m
+# PDF
+sudo pacman -S zathura zathura-pdf-mupdf
+# Productivity
 yaourt -S tasksh tty-clock
+# Media
+yaourt -S playerctl
 
