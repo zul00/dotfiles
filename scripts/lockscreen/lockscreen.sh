60b4dbe8b2fd7852c7538574d203fb512be73483
zul <zul.zulkarnaen@gmail.com>
2017-01-11 09:14:41 +0000

    Updated setting for workspace + i3lockscreen: 1. Allocate more for gen application rather than www 2. Activate lockscreen and suspend using script from reddit 3. Activate printscreen using scrot 4. Update background scaling

---
+++ scripts/lockscreen/lockscreen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ICON=$HOME/scripts/lockscreen/icon/1483940564_lock_locked.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -i $TMPBG
