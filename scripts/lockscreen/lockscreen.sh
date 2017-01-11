3571ddbcee875f5b9844902fb15a3117f647455d
zul <zul.zulkarnaen@gmail.com>
2017-01-11 09:23:02 +0000

    Updated i3status format and setting trackpoint accel at xinitrc

--- scripts/lockscreen/lockscreen.sh
+++ scripts/lockscreen/lockscreen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# source: https://www.reddit.com/r/unixporn/comments/3358vu/i3lock_unixpornworthy_lock_screen/
+# icon source: https://www.iconfinder.com/search/?q=lock&amp;price=premium&amp;license=2
 ICON=$HOME/scripts/lockscreen/icon/1483940564_lock_locked.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
