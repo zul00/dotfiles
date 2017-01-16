7ae52f734d9efcc27748c9761d87eed3407411bb
zul <zul.zulkarnaen@gmail.com>
2017-01-16 13:47:19 +0000

    Updated install app.

--- scripts/install-app.sh
+++ scripts/install-app.sh
@@ -3,10 +3,16 @@
 sudo pacman -S task gvim chromium rxvt-unicode
 # File Manager
 sudo pacman -S ranger w3m
+sudo pacman -S feh scrot
 # PDF
 sudo pacman -S zathura zathura-pdf-mupdf
+# Office
+sudo pacman -S texlive-core texlive-science texlive-langextra
 # Productivity
 yaourt -S tasksh tty-clock
 # Media
 yaourt -S playerctl
+# Programming
+pacman -S ctags
+
 
