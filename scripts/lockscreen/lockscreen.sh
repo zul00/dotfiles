37f7e7062a05810bbe453668e8f1e74792da7648
zul00 <zul.zulkarnaen@gmail.com>
2018-01-13 22:56:04 +0000

    i3lock, finished setting of ffmpeg using awk

--- scripts/lockscreen/lockscreen.sh
+++ scripts/lockscreen/lockscreen.sh
@@ -3,17 +3,18 @@
 # icon source: https://www.iconfinder.com/search/?q=lock&amp;price=premium&amp;license=2
 ICON=$HOME/scripts/lockscreen/icon/1483940564_lock_locked.png
 IMAGE=/tmp/screen.png
+XOFF=`xrandr | awk -F+ '/primary/{print $2}'`
+MAINDISP_W=`xrandr | awk '/primary/{print $4}' | awk -Fx '{print $1}'`
+MAINDISP_H=`xrandr | awk '/primary/{print $4}' | awk -Fx '{print $2}' | awk -F+ '{print $1}'`
 
-param=( --textcolor=ffffff00 --insidecolor=e8dcd000 --ringcolor=e8dcd088 --linecolor=2d283e00 --keyhlcolor=e8dcd0ff --ringvercolor=E9A66Cff --separatorcolor=22222200 --insidevercolor=fadd5c00 --ringwrongcolor=CE325Bff --insidewrongcolor=f1345900 --clock --bar-indicator)
-
-playerctl pause
-
+# Generate Lockscreen
 scrot $IMAGE
 ffmpeg  -loglevel quiet \
         -i $IMAGE \
         -i $ICON \
-        -filter_complex "boxblur=10,overlay=(main_w-overlay_w)/2:(1200-overlay_h)/2" -vframes 1 -y $IMAGE
+        -filter_complex "boxblur=10,overlay=($MAINDISP_W-overlay_w)/2+$XOFF:($MAINDISP_H-overlay_h)/2" -vframes 1 -y $IMAGE
 
-#convert $IMAGE -scale 10% -scale 1000% $IMAGE
-#convert $IMAGE $ICON -gravity center -composite -matte $IMAGE
+# Lock screen
+playerctl pause
+param=( --textcolor=ffffff00 --insidecolor=e8dcd000 --ringcolor=e8dcd088 --linecolor=2d283e00 --keyhlcolor=e8dcd0ff --ringvercolor=E9A66Cff --separatorcolor=22222200 --insidevercolor=fadd5c00 --ringwrongcolor=CE325Bff --insidewrongcolor=f1345900 --clock)
 i3lock "${param[@]}" -i $IMAGE -n &
