8d8174d0af0e87a95dc64ebbd235803cc241b392
zul00 <zul.zulkarnaen@gmail.com>
2018-01-19 09:48:32 +0000

    Fixed ffmpeg steps

--- scripts/lockscreen/lockscreen.sh
+++ scripts/lockscreen/lockscreen.sh
@@ -3,20 +3,25 @@
 # icon source: https://www.iconfinder.com/search/?q=lock&amp;price=premium&amp;license=2
 ICON=$HOME/scripts/lockscreen/icon/1483940564_lock_locked.png
 IMAGE=/tmp/screen.png
+IMAGE2=/tmp/screen2.png
 XOFF=`xrandr | awk -F+ '/primary/{print $2}'`
 MAINDISP_W=`xrandr | awk '/primary/{print $4}' | awk -Fx '{print $1}'`
 MAINDISP_H=`xrandr | awk '/primary/{print $4}' | awk -Fx '{print $2}' | awk -F+ '{print $1}'`
 
 # Generate Lockscreen
 scrot $IMAGE
-## Blur screen capture
-ffmpeg  -loglevel quiet \
-        -i $IMAGE \
-        -i $ICON \
-        -filter_complex "boxblur=10,overlay=($MAINDISP_W-overlay_w)/2+$XOFF:($MAINDISP_H-overlay_h)/2" -vframes 1 -y $IMAGE
 ## Added a box for clock and lock
 ffmpeg  -i $IMAGE \
-        -vf "drawbox=x=$XOFF+25:y=$MAINDISP_H-110:w=300:h=80:color=black@0.7:t=max" -y $IMAGE
+        -vf "drawbox=x=$XOFF+25:y=$MAINDISP_H-110:w=300:h=80:color=black@0.8:t=max" \
+        -y $IMAGE \
+        -loglevel quiet
+
+## Blur screen capture
+ffmpeg   \
+        -i $IMAGE \
+        -i $ICON \
+        -filter_complex "boxblur=10,overlay=($MAINDISP_W-overlay_w)/2+$XOFF:($MAINDISP_H-overlay_h)/2" -vframes 1 -y $IMAGE \
+        -loglevel quiet
 
 # Pause media
 playerctl pause
@@ -36,6 +41,6 @@ i3lock \
 		--keyhlcolor=$letterEnteredColor --bshlcolor=$letterRemovedColor --separatorcolor=$background \
 		--insidevercolor=$passwordCorrect --insidewrongcolor=$passwordIncorrect \
 		--ringvercolor=$foreground --ringwrongcolor=$foreground --indpos="x+280:h-70" \
-		--radius=20 --ring-width=4 --veriftext="" --wrongtext="" \
+		--radius=20 --ring-width=6 --veriftext="" --wrongtext="" \
 		--textcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
         --force-clock
