fec54180f0d6d9d2f13f544a33ebd00ae4d4630b
zul00 <zul.zulkarnaen@gmail.com>
2018-02-21 00:57:24 +0000

    Implement awk on display script

--- scripts/display/display.sh
+++ scripts/display/display.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
-if [ $2 = "right" ]; then
-    xrandr --output $1 --right-of LVDS-1 --auto --rotate normal
-elif [ $2 = "left" ]; then
-    xrandr --output $1 --left-of LVDS-1 --auto --rotate normal
-elif [ $2 = "above" ]; then
-    xrandr --output $1 --above LVDS-1 --auto --rotate normal
-elif [ $2 = "below" ]; then
-    xrandr --output $1 --below LVDS-1 --auto --rotate normal
-elif [ $2 = "same" ]; then
-    xrandr --output $1 --same-as LVDS-1 --auto --rotate normal
-elif [ $2 = "off" ]; then
+
+EXT_DISP=`xrandr | awk '/ connected/ {print $0}' | awk '!/LVDS/ {print $1}'`
+
+if [ $1 = "right" ]; then
+    xrandr --output $EXT_DISP --right-of LVDS-1 --auto --rotate normal
+elif [ $1 = "left" ]; then
+    xrandr --output $EXT_DISP --left-of LVDS-1 --auto --rotate normal
+elif [ $1 = "above" ]; then
+    xrandr --output $EXT_DISP --above LVDS-1 --auto --rotate normal
+elif [ $1 = "below" ]; then
+    xrandr --output $EXT_DISP --below LVDS-1 --auto --rotate normal
+elif [ $1 = "same" ]; then
+    xrandr --output $EXT_DISP --same-as LVDS-1 --auto --rotate normal
+elif [ $1 = "off" ]; then
+    EXT_DISP=`xrandr | awk '/+/ {print $0}' | awk '/mm/ {print $0}' | awk '!/LVDS/ {print $1}'`
     display=`xrandr | grep connected | grep -v LVDS | grep mm | egrep -o '[A-Z]{4,}-[1-4]{1}'`
-    xrandr --output $display --off
+    xrandr --output $display --off --output LVDS-1 --auto
 fi&
 
-if [ $2 = "off" ]; then
+if [ $1 = "off" ]; then
     pacmd set-card-profile 0 "output:analog-stereo+input:analog-stereo"
 else
     pacmd set-card-profile 0 "output:hdmi-stereo+input:analog-stereo"
