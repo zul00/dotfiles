be47f5099176855298943ec5b29f723457aefb9c
zul00 <zul.zulkarnaen@gmail.com>
2018-03-17 06:06:05 +0100

    Removed unnecessary stuffs

--- scripts/display/toggle_display.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-display=`xrandr | grep -w connected | egrep -v LVDS | egrep -o '[A-Z]{3,}-[1-4]{1}'`
-echo $display
-if [[ -z "$display" ]]; then
-    ~/scripts/display/display.sh "empty" $1
-else
-    ~/scripts/display/display.sh $display $1
-fi
