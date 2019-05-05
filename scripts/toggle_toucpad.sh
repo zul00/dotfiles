2d4250dcf2d5c13b760ac59f377a2f4789462318
zul00 <zul.zulkarnaen@gmail.com>
2019-05-05 13:27:02 +0200

    Added notification for toggling touchpad

--- scripts/toggle_toucpad.sh
+++ scripts/toggle_toucpad.sh
@@ -5,7 +5,7 @@ device=`xinput list | grep TouchPad | egrep -o 'id=[0-9]?{3}' |\
 state=`xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$"`
 
 if [ $state == '1' ];then
-    xinput --disable $device
+    xinput --disable $device && notify-send "Touchpad disabled" -u low
 else
-    xinput --enable $device
+    xinput --enable $device && notify-send "Touchpad enabled" -u low
 fi
