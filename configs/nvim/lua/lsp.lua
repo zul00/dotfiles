dd4ad69779ff696668d6d2f567e44385c4a1a856
zul <zul.zulkarnaen@gmail.com>
2021-10-27 07:58:27 +0200

    Added pylsp

--- configs/nvim/lua/lsp.lua
+++ configs/nvim/lua/lsp.lua
@@ -1,10 +1,39 @@
-
 local lspconfig = require'lspconfig'
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
 
 require('lspconfig').ccls.setup{
     on_attach=require'completion'.on_attach;
     root_dir = lspconfig.util.root_pattern(".ccls");
     init_options = { cache = { directory= ".ccls-cache"; } }
+}
+
+configs.pylsp = {
+  on_attach=require'completion'.on_attach;
+  default_config = {
+    cmd = { 'pylsp' },
+    filetypes = { 'python' },
+    root_dir = function(fname)
+      local root_files = {
+        'pyproject.toml',
+        'setup.py',
+        'setup.cfg',
+        'requirements.txt',
+        'Pipfile',
+        '.git',
+      }
+      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+    end,
+  },
+  docs = {
+    description = [[
+https://github.com/python-lsp/python-lsp-server
+A Python 3.6+ implementation of the Language Server Protocol.
+The language server can be installed via `pipx install 'python-lsp-server[all]'`.
+Further instructions can be found in the [project's README](https://github.com/python-lsp/python-lsp-server).
+Note: This is a community fork of `pyls`.
+    ]],
+  },
 }
 
 require('lspconfig').tsserver.setup{
