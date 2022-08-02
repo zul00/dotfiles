f7d72bd41015b7d055fa4d3e779e888e7751decb
zul <zul.zulkarnaen@gmail.com>
2022-08-02 19:13:13 +0200

    Added packer and fugitive

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,2 +1,22 @@
 require("configs")
 print("Hello from init.lua")
+
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- stylua: ignore start
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'                                                    -- Package manager
+  use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
+  
+  if is_bootstrap then
+    require('packer').sync()
+  end
+end)
+-- stylua: ignore end
