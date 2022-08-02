789e829a1c84eb42dd64d22b31525efb7c95c1f7
zul <zul.zulkarnaen@gmail.com>
2022-08-02 21:41:12 +0200

    Added remap.lua

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,5 +1,7 @@
 require("configs")
 require("style")
+
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 
 require("plugins")
@@ -8,14 +10,22 @@ print("Hello from init.lua")
 
 -- Temporary place for keymaps
 -- Files navigation
-vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<C-f>', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
-vim.keymap.set('n', '<C-g>', require('telescope.builtin').git_files, { desc = '[S]earch [G]uffers [F]iles' })
+local Remap = require("remap")
+local nnoremap = Remap.nnoremap
+local vnoremap = Remap.vnoremap
+local inoremap = Remap.inoremap
+local xnoremap = Remap.xnoremap
+local nmap = Remap.nmap
+
+nnoremap('<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+nnoremap('<C-f>', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
+nnoremap('<C-g>', require('telescope.builtin').git_files, { desc = '[S]earch [G]uffers [F]iles' })
 
 -- Git navigation
-vim.keymap.set('n', '<leader>gs', "<cmd>Git<cr>", { silent = true })
-vim.keymap.set('n', '<leader>gl', "<cmd>GV<cr>", { silent = true })
+nnoremap('<leader>gs', "<cmd>Git<cr>", { silent = true })
+nnoremap('<leader>gl', "<cmd>GV<cr>", { silent = true })
+nnoremap('<leader>gd', "<cmd>Gvdiffsplit<cr>", { silent = true })
 
 -- Others
-vim.keymap.set('n', '<leader>h', "<cmd>noh<cr>", { silent = true })
-vim.keymap.set('n', '<leader>r', require('telescope.builtin').live_grep, { desc = '[G]rep [S]trings' })
+nnoremap('<leader>h', "<cmd>noh<cr>", { silent = true })
+nnoremap('<leader>r', require('telescope.builtin').live_grep, { desc = '[G]rep [S]trings' })
---
+++ configs/nvim/lua/remap.lua
@@ -0,0 +1,21 @@
+-- Source: https://github.com/ThePrimeagen/.dotfiles/blob/master/vim-2022/.config/nvim/lua/theprimeagen/remap.lua
+local M = {}
+
+function bind(op, outer_opts)
+    outer_opts = outer_opts or {noremap = true}
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
+end
+
+M.nmap = bind("n", {noremap = false})
+M.nnoremap = bind("n")
+M.vnoremap = bind("v")
+M.xnoremap = bind("x")
+M.inoremap = bind("i")
+
+return M
