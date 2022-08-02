a2e80b9f36790659b2a5feee6b8771619e5d8321
zul <zul.zulkarnaen@gmail.com>
2022-08-02 19:16:50 +0200

    Added bootstrap for packer

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,22 +1,21 @@
 require("configs")
 print("Hello from init.lua")
 
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+-- Bootstrap packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
 end
 
--- stylua: ignore start
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'                                                    -- Package manager
-  use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
-  
-  if is_bootstrap then
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'tpope/vim-fugitive'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
     require('packer').sync()
   end
 end)
--- stylua: ignore end
