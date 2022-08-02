7226ab568d703d8112f08379cf86b3e88da31f7f
zul <zul.zulkarnaen@gmail.com>
2022-08-02 20:41:37 +0200

    Added some basic shortcut

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,8 +1,12 @@
 require("configs")
+require("style")
+vim.g.mapleader = ','
+
 require("plugins")
 print("Hello from init.lua")
 
--- Theme
-vim.cmd [[colorscheme gruvbox]]
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>g', "<cmd>Git<cr>", { silent = true })
+vim.keymap.set('n', '<leader>gl', "<cmd>GV<cr>", { silent = true })
+vim.keymap.set('n', '<leader>h', "<cmd>noh<cr>", { silent = true })
---
+++ configs/nvim/lua/style.lua
@@ -0,0 +1,11 @@
+print("Hello from style.lua")
+
+-- Theme
+vim.cmd [[colorscheme gruvbox]]
+
+-- Formatting
+vim.cmd [[set expandtab]]
+vim.cmd [[set shiftwidth=4]]
+vim.cmd [[set tabstop=4]]
+vim.cmd [[set autoindent]]
+vim.cmd [[set smarttab]]
