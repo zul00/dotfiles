7e3951475d5649803c5eb626187dce249f983e2e
zul <zul.zulkarnaen@gmail.com>
2022-08-02 19:25:42 +0200

    Moved plugins configuration to plugins.lua

---
+++ configs/nvim/lua/plugins.lua
@@ -0,0 +1,38 @@
+print("Hello from plugins.lua")
+
+-- Bootstrap packer
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Git good
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
+  use 'shumphrey/fugitive-gitlab.vim'
+  use 'tommcdo/vim-fubitive'
+  use 'junegunn/gv.vim'
+  use {'sindrets/diffview.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'lewis6991/gitsigns.nvim'
+
+  -- tpope's
+  use 'tpope/vim-surround'
+  use 'tpope/vim-vinegar'
+  use 'tpope/vim-dispatch'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-unimpaired'
+  use 'tpope/vim-eunuch'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
