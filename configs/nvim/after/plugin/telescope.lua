fae169d42399b91efdfcf20f6bc9e50f733730f2
Zulkarnaen <zul@twtg.io>
2022-08-04 00:56:21 +0200

    Updated telescope configuration

--- configs/nvim/after/plugin/telescope.lua
+++ configs/nvim/after/plugin/telescope.lua
@@ -1,5 +1,15 @@
-require('telescope').setup {}
+require('telescope').setup {
+    defaults = {
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            prompt_position = "top",
+            preview_cutoff = 20
+        },
+    }
+}
 
+-- EXTENSIONS
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 require('telescope').load_extension('fzf')
