ba1992d5db3aa0bdd2718aab5b0a3e15338568bb
zul <zul.zulkarnaen@gmail.com>
2022-08-14 12:09:15 +0200

    Assign localleader

--- configs/nvim/lua/configs/vim_basics.lua
+++ configs/nvim/lua/configs/vim_basics.lua
@@ -33,6 +33,7 @@ vim.o.completeopt = 'menuone,noselect'
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Show trailing space
 vim.o.list = true
