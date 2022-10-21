88b438ff5b13acb7e8d480010341562ffd1eb0a9
Zulkarnaen <zul@twtg.io>
2022-10-21 07:54:14 +0200

    Added serial_launcher script

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -7,3 +7,5 @@ require("configs.theme")
 vim.g['localvimrc_sandbox'] = 0
 vim.g['localvimrc_whitelist'] = '*/.lvimrc'
 require("luasnip.loaders.from_vscode").lazy_load()
+
+-- vim.lsp.set_log_level("debug")
