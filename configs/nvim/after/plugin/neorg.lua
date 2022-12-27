58ef6cf0223f663e9ddf954b8376cbbdb6e40149
Zulkarnaen <zul.zulkarnaen@gmail.com>
2022-12-27 10:04:31 +0400

    Updated to neorg config

--- configs/nvim/after/plugin/neorg.lua
+++ configs/nvim/after/plugin/neorg.lua
@@ -5,6 +5,7 @@ require('neorg').setup {
             config = {
                 workspaces = {
                     twtg = "~/Documents/notes/twtg",
+                    tii = "~/Documents/notes/tii",
                     private = "~/Documents/notes/private",
                 }
             }
@@ -16,6 +17,16 @@ require('neorg').setup {
         },
         ["core.integrations.telescope"] = {},
         ["core.norg.qol.toc"] = {
+            config = { -- Note that this table is optional and doesn't need to be provided
+                -- Configuration here
+            }
+        },
+        ["core.export"] = {
+            config = { -- Note that this table is optional and doesn't need to be provided
+                -- Configuration here
+            }
+        },
+        ["core.export.markdown"] = {
             config = { -- Note that this table is optional and doesn't need to be provided
                 -- Configuration here
             }
