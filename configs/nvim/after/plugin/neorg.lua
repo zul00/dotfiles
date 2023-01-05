490efdf72f68b7e207f182d6c8749cb47017401b
Zulkarnaen <zul.zulkarnaen@gmail.com>
2023-01-05 07:46:38 +0400

    neorg: Updated treesitter integration

--- configs/nvim/after/plugin/neorg.lua
+++ configs/nvim/after/plugin/neorg.lua
@@ -38,10 +38,3 @@ require('neorg').setup {
         }
     }
 }
-
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { "norg", --[[ other parsers you would wish to have ]] },
-    highlight = { -- Be sure to enable highlights if you haven't!
-        enable = true,
-    }
-}
