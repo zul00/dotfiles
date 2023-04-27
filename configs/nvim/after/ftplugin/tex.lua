b6401c136690f6ce31b21495fb35055c50ea82e6
Zulkarnaen <zul.zulkarnaen@gmail.com>
2023-04-27 10:13:30 +0400

    Change snippet source

--- configs/nvim/after/ftplugin/tex.lua
+++ configs/nvim/after/ftplugin/tex.lua
@@ -6,3 +6,7 @@ vim.g.vimtex_fold_enabled = 1
 
 -- Editor configuration
 vim.opt.foldmethod = 'expr'
+
+-- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-cool-snippets" } })
+-- TODO: Replace everything with snipmate!
+require("luasnip.loaders.from_snipmate").lazy_load()
