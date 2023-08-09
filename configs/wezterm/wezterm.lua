8ea54f80c32e2bad8e80e5643809d4b6f8496791
Zulkarnaen <zul.zulkarnaen@gmail.com>
2023-08-09 14:57:27 +0400

    Updated wezterm to match the "current style"

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -13,17 +13,28 @@ end
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- Font configs
 config.font = wezterm.font {
     family = 'JetBrainsMono Nerd Font Mono',
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 }
+config.font_size = 10.0
+-- config.force_reverse_video_cursor = true
+-- config.bold_brightens_ansi_colors = true
+
+-- Color configs
 config.color_scheme = 'GruvboxDark'
+config.bold_brightens_ansi_colors = "BrightAndBold"
+config.window_background_opacity = 0.97
 
 -- Other configs
 config.enable_tab_bar = false
-config.bold_brightens_ansi_colors = "BrightAndBold"
-config.window_background_opacity = 0.97
+config.window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+}
 
 -- and finally, return the configuration to wezterm
 return config
