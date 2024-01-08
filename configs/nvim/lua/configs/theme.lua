a9d3467bf62e18949d09e99083bda5d5c260676e
Zulkarnaen <zul.zulkarnaen@gmail.com>
2024-01-08 09:22:48 +0400

    Updated formatting on theme.lua

--- configs/nvim/lua/configs/theme.lua
+++ configs/nvim/lua/configs/theme.lua
@@ -52,12 +52,12 @@ require("todo-comments").setup {
         NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     },
     highlight = {
-        before = "", -- "fg" or "bg" or empty
-        keyword = "bg", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-        after = "fg", -- "fg" or "bg" or empty
+        before = "",                     -- "fg" or "bg" or empty
+        keyword = "bg",                  -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+        after = "fg",                    -- "fg" or "bg" or empty
         pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-        comments_only = true, -- uses treesitter to match keywords in comments only
-        max_line_len = 400, -- ignore lines longer than this
-        exclude = {}, -- list of file types to exclude highlighting
+        comments_only = true,            -- uses treesitter to match keywords in comments only
+        max_line_len = 400,              -- ignore lines longer than this
+        exclude = {},                    -- list of file types to exclude highlighting
     },
 }
