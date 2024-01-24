9a0be3be487e39d5edaa45c1f40bfa2e6a04db55
Zulkarnaen <zul.zulkarnaen@gmail.com>
2024-01-24 14:16:39 +0400

    Updated neorg config

--- configs/nvim/after/ftplugin/norg.lua
+++ configs/nvim/after/ftplugin/norg.lua
@@ -1,3 +1,7 @@
 local nnoremap = require("remap").nnoremap
+
 nnoremap('<C-s>', '<cmd>Telescope neorg switch_workspace<CR>', { silent = true })
 nnoremap('<C-h>', '<cmd>Telescope neorg find_linkable<CR>', { silent = true })
+
+-- Conceal URL
+vim.o.conceallevel=2
