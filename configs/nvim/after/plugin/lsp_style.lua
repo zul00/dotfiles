d013be36fca12127ed809f1c0c7959a9ec03910c
Zulkarnaen <zul.zulkarnaen@gmail.com>
2025-04-24 06:34:31 +0400

    Updated deprecated syntax

--- configs/nvim/after/plugin/lsp_style.lua
+++ configs/nvim/after/plugin/lsp_style.lua
@@ -1,9 +1,18 @@
 -- {Icon in gutter}
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN]  = "",
+      [vim.diagnostic.severity.HINT]  = "",
+      [vim.diagnostic.severity.INFO]  = "",
+    },
+  },
+  underline = true,
+  virtual_text = false,
+  update_in_insert = false,
+  severity_sort = true,
+})
 
 require("trouble").setup {}
 
