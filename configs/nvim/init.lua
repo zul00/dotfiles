d902da7c39215c200dde7a69aec8792ab67f57f0
Zulkarnaen <zul.zulkarnaen@gmail.com>
2025-04-26 09:46:33 +0400

    Cleanup, removed unnecessary function
    plugin config under after/plugin are automatically loaded

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,28 +1,17 @@
 -- Helper functions
 function PrintCurrentFileName()
-    local info = debug.getinfo(2, "S") -- use level 2 to get the caller's file
+    local info = debug.getinfo(2, "S")   -- use level 2 to get the caller's file
     local full_path = info.source:sub(2) -- remove '@'
     local file_name = full_path:match("^.+/(.+)$") or full_path
     print("Loading file: " .. file_name)
 end
 
-local function load_modules_from(dir)
-    local path = vim.fn.stdpath("config") .. "/lua/" .. dir
-    for _, file in ipairs(vim.fn.readdir(path)) do
-        if file:match("%.lua$") then
-            local module = dir:gsub("/", ".") .. "." .. file:gsub("%.lua$", "")
-            require(module)
-        end
-    end
-end
-
-
 -- Load basic configuration
 require("configs.vim_basics")
 
-load_modules_from("configs")
+-- Load plugin loader
+-- NOTE: plugin configuration are automatically loaded from ./after/plugin
 require("plugins")
-load_modules_from("plugins")
 
 -- REVIEW: What was it for again?
 require("luasnip.loaders.from_vscode").lazy_load()
